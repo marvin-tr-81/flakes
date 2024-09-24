485cbe14c5b9d7a1096988b35369f1c1a602b245
marvin <oilkerbasaran@gmail.com>
2024-09-24 12:50:13 +0300

    modular home-manager

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
         ./nixosModules
         inputs.nixos-hardware.nixosModules.framework-11th-gen-intel
         home-manager.nixosModules.home-manager
+        stylix.nixosModules.stylix
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
