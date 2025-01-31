f23dcda421d3002c3f8d5b1426cf76dca1a6d57e
marvin <oilkerbasaran@gmail.com>
2025-01-31 17:33:19 +0300

    vim options added

--- nixosModules/nixvim/core/options.nix
+++ nixosModules/nixvim/core/options.nix
@@ -8,16 +8,43 @@
 
     opts = {
 
+      # line numbers
       relativenumber = true;
       number = true;
 
-      spell = false;
+      # spelling and wrapping
+      spell = true;
       wrap = false;
 
+      # tabs and indentation
       tabstop = 2;
       shiftwidth = 2;
       expandtab = true;
       autoindent = true;
+
+      # search settings
+      ignorecase = true;
+      smartcase = true;
+
+      # cursor line
+      cursorline = true;
+
+      # sign column
+      termguicolors = true;
+      signcolumn = "yes";
+
+      # backspace
+      backspace = "indent,eol,start";
+
+      # clipboard
+      clipboard = "unnamedplus";
+
+      # split window
+      splitright = true;
+      splitbelow = true;
+
+      # turn off swap file
+      swapfile = false;
 
     };
   };
