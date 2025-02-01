b0491a197a8e1402b91c5cd18cf2469b1c250380
marvin <oilkerbasaran@gmail.com>
2025-02-01 18:29:42 +0300

    which-key plugin added

--- nixosModules/nixvim/plugins/default.nix
+++ nixosModules/nixvim/plugins/default.nix
@@ -1,10 +1,15 @@
 {
+
   imports = [
+
     ./web-devicons.nix
     ./tmux-navigator.nix
     ./nvim-tree.nix
+    ./which-key.nix
     #./lualine.nix
     #./bufferline.nix
     #./lightline.nix
+
   ];
+
 }
