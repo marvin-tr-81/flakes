8ac853aa7df9ad184ef40dac729bc143a1fee0c0
marvin <oilkerbasaran@gmail.com>
2025-02-26 17:25:00 +0300

    kanata configured with ralt and zsa keymapp now works

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -9,25 +9,28 @@
         extraDefCfg = "process-unmapped-keys yes";
         config = ''
           (defsrc
-           a s d f j k l ;
+           caps a s d f j k l ; x .
           )
           (defvar
            tap-time 150
            hold-time 200
           )
           (defalias
+           caps esc
            a (tap-hold $tap-time $hold-time a lmet)
            s (tap-hold $tap-time $hold-time s lalt)
            d (tap-hold $tap-time $hold-time d lsft)
            f (tap-hold $tap-time $hold-time f lctl)
            j (tap-hold $tap-time $hold-time j rctl)
            k (tap-hold $tap-time $hold-time k rsft)
-           l (tap-hold $tap-time $hold-time l ralt)
+           l (tap-hold $tap-time $hold-time l lalt)
            ; (tap-hold $tap-time $hold-time ; rmet)
+           x (tap-hold $tap-time $hold-time x ralt)
+           . (tap-hold $tap-time $hold-time . ralt)
           )
 
           (deflayer base
-           @a  @s  @d  @f  @j  @k  @l  @;
+           @caps @a  @s  @d  @f  @j  @k  @l  @; @x @.
           )
         '';
       };
