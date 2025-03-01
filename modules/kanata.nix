1ea7e71a6dfa11c095c6b9782eeb79ee21f4c851
marvin <oilkerbasaran@gmail.com>
2025-03-01 18:28:20 +0300

    keyboard customized for ~ key in kanata

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -1,25 +1,35 @@
 {
   services.kanata = {
+
     enable = true;
+
     keyboards = {
+
       internalKeyboard = {
+
         devices = [
           "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
         ];
+
         extraDefCfg = "process-unmapped-keys yes";
+
         config = ''
           (defsrc
-           caps
+           caps < `
            a s d f
            j k l ;
            x . c ,
           )
+
           (defvar
            tap-time 150
            hold-time 200
           )
+
           (defalias
            caps esc
+           < `
+           ` =
            a (tap-hold $tap-time $hold-time a lmet)
            s (tap-hold $tap-time $hold-time s lalt)
            d (tap-hold $tap-time $hold-time d lsft)
@@ -35,9 +45,10 @@
           )
 
           (deflayer base
-           @caps @a  @s  @d  @f  @j  @k  @l  @; @x @. @c @,
+           @caps @< @` @a  @s  @d  @f  @j  @k  @l  @; @x @. @c @,
           )
         '';
+
       };
     };
   };
