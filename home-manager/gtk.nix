3bbff42b6d63c36f6d28068af0eba1e2bafad9a5
marvin <oilkerbasaran@gmail.com>
2025-08-04 12:38:21 +0300

    remove gtk theme

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -1,5 +1,28 @@
+{ pkgs, config, ... }:
 {
 
-  gtk.enable = true;
+  gtk = {
+    enable = true;
+
+    # theme = {
+    #   name = "Catppuccin-Frappe";
+    #   package = pkgs.catppuccin-gtk.override {
+    #     accents = [ "mauve" ];
+    #     tweaks = [ ];
+    #     variant = "frappe";
+    #   };
+    # };
+
+  };
+
+  # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+  # xdg.configFile = {
+  #   "gtk-4.0/assets".source =
+  #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+  #   "gtk-4.0/gtk.css".source =
+  #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+  #   "gtk-4.0/gtk-dark.css".source =
+  #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  # };
 
 }
