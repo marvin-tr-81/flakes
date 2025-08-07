b3dde25c2e1b082d8349c6e2ae46e5cb8bfa766f
marvin <oilkerbasaran@gmail.com>
2025-08-07 22:43:37 +0300

    configure hyprbars

--- home-manager/hyprland/plugins/hyprbars.nix
+++ home-manager/hyprland/plugins/hyprbars.nix
@@ -3,12 +3,12 @@
 
     settings = {
 
-      plugin.hyprbars.bar_height = 20;
+      plugin.hyprbars.bar_height = 30;
       plugin.hyprbars.hyprbars-button = [
-        "rgb(ff4040), 10, 󰖭, hyprctl dispatch killactive"
-        "rgb(eeee11), 10, , hyprctl dispatch fullscreen 1"
+        # "rgb(ff4040), 10, 󰖭, hyprctl dispatch killactive"
+        # "rgb(eeee11), 10, , hyprctl dispatch fullscreen 1"
       ];
-      plugin.hyprbars.on_double_click = "hyprctl dispatch fullscreen 1";
+      # plugin.hyprbars.on_double_click = "hyprctl dispatch fullscreen 1";
 
     };
 
