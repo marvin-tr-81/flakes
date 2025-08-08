174b3f6676383ab04e590914ea47e99ea2183078
marvin <oilkerbasaran@gmail.com>
2025-08-08 14:21:42 +0300

    fix gtk theme catppuccin-frappe

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -1,47 +1,26 @@
 { pkgs, ... }:
 {
-
   gtk = {
+
     enable = true;
 
     theme = {
-      # name = "Graphite-Dark";
-      # package = pkgs.graphite-gtk-theme.override {
-      # tweaks = [ "darker" ];
-      # themeVariants = [ "pink" ];
-      # colorVariants = [ "dark" ];
-      # };
-      name = "catppuccin-mocha-blue-compact+default";
+      name = "catppuccin-frappe-mauve-compact";
       package = pkgs.catppuccin-gtk.override {
-        accents = [ "blue" ];
+        accents = [ "mauve" ];
         size = "compact";
-        tweaks = [ "black" ];
-        variant = "mocha";
+        # tweaks = [ "black" ];
+        variant = "frappe";
       };
     };
+
+    # iconTheme = {
+    #   name = "Tela-circle-dark";
+    #   package = pkgs.tela-circle-icon-theme;
+    # };
+
     gtk3.extraConfig.Settings = ''gtk-application-prefer-dark-themes=1'';
     gtk4.extraConfig.Settings = ''gtk-application-prefer-dark-themes=1'';
+
   };
-
-  # theme = {
-  #   name = "Catppuccin-Frappe";
-  #   package = pkgs.catppuccin-gtk.override {
-  #     accents = [ "mauve" ];
-  #     tweaks = [ ];
-  #     variant = "frappe";
-  #   };
-  # };
-
-  # };
-
-  # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
-  # xdg.configFile = {
-  #   "gtk-4.0/assets".source =
-  #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-  #   "gtk-4.0/gtk.css".source =
-  #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-  #   "gtk-4.0/gtk-dark.css".source =
-  #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-  # };
-
 }
