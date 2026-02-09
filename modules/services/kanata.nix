7284e28c8877f7724a0cb365c893bbd2e15f775a
marvin <oilkerbasaran@gmail.com>
2026-02-09 16:04:31 +0300

    update kanata config, set s to meta and a to ralt

--- modules/services/kanata.nix
+++ modules/services/kanata.nix
@@ -31,15 +31,16 @@
           (defalias
             ;; base layer aliases
             grv  (tap-hold $tap-time $hold-time grv (layer-toggle layers))
-            a    (tap-hold $tap-time $hold-time a lalt)
-            s    (tap-hold $tap-time $hold-time s ralt)
+            a    (tap-hold $tap-time $hold-time a ralt)
+            s    (tap-hold $tap-time $hold-time s lmet)
             d    (tap-hold $tap-time $hold-time d lsft)
             f    (tap-hold $tap-time $hold-time f lctl)
+            g    (tap-hold $tap-time $hold-time g lalt)
+            h    (tap-hold $tap-time $hold-time h lalt)
             j    (tap-hold $tap-time $hold-time j rctl)
             k    (tap-hold $tap-time $hold-time k rsft)
-            l    (tap-hold $tap-time $hold-time l ralt)
-            ;    (tap-hold $tap-time $hold-time ; lalt)
-            spc  (tap-hold $tap-time $hold-time spc lmet)
+            l    (tap-hold $tap-time $hold-time l rmet)
+            ;    (tap-hold $tap-time $hold-time ; ralt)
             lctl (layer-toggle nmpd)
             lalt (tap-hold $tap-time $hold-time bspc (layer-toggle sys))
             ralt (tap-hold $tap-time $hold-time ret (layer-toggle sys))
@@ -49,9 +50,9 @@
             esc f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 del
             @grv 1  2  3  4  5  6  7  8  9  0  -  =  bspc
             tab  q  w  e  r  t  y  u  i  o  p  [  ]  ret
-            esc   @a @s @d @f g  h  @j @k @l @; '  \
+            esc   @a @s @d @f @g @h @j @k @l @; '  \
             lsft ` z  x  c  v  b  n  m  ,  .  /     rsft
-            @lctl lmet @lalt   @spc    @ralt  rctl
+            @lctl lmet @lalt   spc     @ralt  rctl
           )
 
           (deflayer raw
@@ -81,12 +82,12 @@
 
           (deflayer nmpd
             ;; numpad layer
-            esc f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 NumLock
-            @grv 1  2  3  4  5 XX kp7 kp8 kp9 kp/ XX   XX  bspc
-            tab  q  w  e  r  t  XX kp4 kp5 kp6 kp* NumpadEqual XX kprt 
+            esc f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 del
+            @grv 1  2  3  4  5 NumLock kp7 kp8 kp9 kp/ XX   XX  bspc
+            tab  q  w  e  r  t  XX kp4 kp5 kp6 kp* NumpadEqual XX ret 
             caps  a  s  d  f  g  XX kp1 kp2 kp3 kp- XX   XX
-            lsft < z  x  c  v  b  XX kp0 kp, kp. kp+      rsft
-            lctl  lmet  bspc    spc     ret  rctl
+            lsft < z  x  c  v  b  XX kp0 NumpadEqual kp. kp+ rsft
+            lctl  lmet  bspc    spc     kprt  rctl
           )
 
           (defalias
