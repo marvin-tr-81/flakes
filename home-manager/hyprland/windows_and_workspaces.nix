48b8a3f699eec3aa1338ec5f43a42a6f1bcc6042
marvin <oilkerbasaran@gmail.com>
2026-03-14 19:40:56 +0300

    remove home-manager hyprland windowrulev2

--- home-manager/hyprland/windows_and_workspaces.nix
+++ home-manager/hyprland/windows_and_workspaces.nix
@@ -22,24 +22,24 @@
 
     ];
 
-    windowrulev2 = [
+    # windowrulev2 = [
 
-      # Fix some dragging issues with XWayland
-      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+    # Fix some dragging issues with XWayland
+    # "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
 
-      # float Youtube Picture in picture
-      "float, title:Picture-in-Picture$"
+    # float Youtube Picture in picture
+    # "float, title:Picture-in-Picture$"
 
-      # pin Youtube Picture in picture
-      "pin, title:Picture-in-Picture$"
+    # pin Youtube Picture in picture
+    # "pin, title:Picture-in-Picture$"
 
-      # hyprbars nobar except dosbox-staging
-      "plugin:hyprbars:nobar, class:negative:org.dosbox-staging.dosbox-staging"
+    # hyprbars nobar except dosbox-staging
+    # "plugin:hyprbars:nobar, class:negative:org.dosbox-staging.dosbox-staging"
 
-      # float dosbox-staging
-      "float, class:org.dosbox-staging.dosbox-staging"
+    # float dosbox-staging
+    # "float, class:org.dosbox-staging.dosbox-staging"
 
-    ];
+    # ];
 
   };
 }
