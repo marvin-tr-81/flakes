85f2e610716b112e754f7beb374741f58e45e0e8
marvin <oilkerbasaran@gmail.com>
2026-03-27 19:43:17 +0300

    remove nixvim plugin telescope

--- modules/nixvim/plugins/default.nix
+++ modules/nixvim/plugins/default.nix
@@ -2,19 +2,19 @@
 
   imports = [
 
-    ./lz-n.nix    # required for Lazy Loading
+    ./lz-n.nix # required for Lazy Loading
     ./web-devicons.nix
     ./tmux-navigator.nix
     ./nvim-tree.nix
     ./which-key.nix
-    ./telescope.nix
+    # ./telescope.nix
     ./auto-session.nix
     ./bufferline.nix
     ./lualine.nix
     ./dressing.nix
     ./treesitter.nix
     ./indent-blankline.nix
-    ./cmp.nix               # completion
+    ./cmp.nix # completion
     ./nvim-autopairs.nix
     ./comment.nix
     ./nvim-surround.nix
