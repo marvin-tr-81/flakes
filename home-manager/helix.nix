d1aed75d66164b0751c361cfaa851f308850c1a8
marvin <oilkerbasaran@gmail.com>
2026-04-22 20:29:20 +0300

    add home-manager helix lsp python, nix

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -1,5 +1,39 @@
+{ pkgs, ... }:
 {
+
   programs.helix = {
     enable = true;
+    languages = {
+      language = [
+        {
+          name = "python";
+          language-servers = [
+            "pyright"
+            "ruff"
+          ];
+        }
+        {
+          name = "nix";
+          language-servers = [ "nixd" ];
+        }
+      ];
+
+      language-server = {
+        pyright.command = "${pkgs.pyright}/bin/pyright-langserver";
+        pyright.args = [ "--stdio" ];
+
+        nixd.command = "${pkgs.nixd}/bin/nixd";
+
+        ruff.command = "${pkgs.ruff}/bin/ruff";
+        ruff.args = [ "server" ];
+      };
+    };
   };
+
+  home.packages = with pkgs; [
+    pyright # Python LSP
+    nixd # Nix LSP
+    ruff # Python formatter/linter
+  ];
+
 }
