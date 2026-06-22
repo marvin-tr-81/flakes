f4002bd5ca2e64610b1bbd6ff6075bd11eadb719
marvin <marvin@example.com>
2026-06-22 15:14:28 +0300

    initial configuration

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "marvin's flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }@inputs: {
+    nixosConfigurations = {
+      marvin-framework = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
