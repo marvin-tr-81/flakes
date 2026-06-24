20e6fcc23d25e4ec62291aa517338326b5d98ce9
marvin <marvin@example.com>
2026-06-24 16:47:20 +0300

    add nixos-hardware

--- flake.nix
+++ flake.nix
@@ -3,14 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }@inputs: {
+  outputs = { self, nixpkgs, nixos-hardware }@inputs: {
     nixosConfigurations = {
       marvin-framework = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          nixos-hardware.nixosModules.framework-11th-gen-intel
         ];
       };
     };
