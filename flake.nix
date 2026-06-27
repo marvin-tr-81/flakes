1b243729ba7da157e3a7a9f9cf26fab187bb215b
marvin <marvin@example.com>
2026-06-27 15:16:43 +0300

    add home-manager as a module

--- flake.nix
+++ flake.nix
@@ -3,19 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware }@inputs: {
     nixosConfigurations = {
       marvin-framework = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
           nixos-hardware.nixosModules.framework-11th-gen-intel
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+
+            home-manager.users.marvin = import ./home-manager;
+          }
         ];
       };
     };
---
+++ home-manager/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    
+  ];
+
+  home.username = "marvin";
+  home.homeDirectory = "/home/marvin";
+  home.stateVersion = "26.05";
+
+  programs.home-manager.enable = true;
+}
