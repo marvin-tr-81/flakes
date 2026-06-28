05ca0f7d8966786a45f0e4246116a670b149c92c
marvin <marvin@example.com>
2026-06-28 16:04:37 +0300

    move configuration.nix to hosts/

--- flake.nix
+++ flake.nix
@@ -8,32 +8,39 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware }@inputs: {
-    nixosConfigurations = {
-      marvin-framework = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          nixos-hardware.nixosModules.framework-11th-gen-intel
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-hardware,
+    }@inputs:
+    {
+      nixosConfigurations = {
+        marvin-framework = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/marvin-framework
+            nixos-hardware.nixosModules.framework-11th-gen-intel
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
 
-            home-manager.users.marvin = import ./home-manager/marvin;
-            home-manager.users.root = import ./home-manager/root;
-          }
-        ];
+              home-manager.users.marvin = import ./home-manager/marvin;
+              home-manager.users.root = import ./home-manager/root;
+            }
+          ];
+        };
       };
     };
-  };
 }
