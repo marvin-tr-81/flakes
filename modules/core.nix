41a6b818c2390061f0d0c0e77718d2f057f2b8b5
marvin <marvin@example.com>
2026-07-10 14:56:25 +0300

    enable service syncthing

--- modules/core.nix
+++ modules/core.nix
@@ -61,6 +61,16 @@
     git
   ];
 
+  services = {
+    syncthing = {
+      enable = true;
+      user = "marvin";
+      dataDir = "/home/marvin/Documents";
+      configDir = "/home/marvin/Documents/.config/syncthing";
+      openDefaultPorts = true;
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
