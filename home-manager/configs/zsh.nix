dfd4fd9a560529a5fefed4a562ff18b94f60798c
marvin <marvin@example.com>
2026-08-07 15:39:19 +0300

    fix home-manager zsh backspace

--- home-manager/configs/zsh.nix
+++ home-manager/configs/zsh.nix
@@ -10,5 +10,11 @@
       la = "ls -lha";
       ll = "ls -lh";
     };
+
+    initContent = ''
+      # Allow backspace over existing text and line breaks in Vi insert mode
+      bindkey -M viins '^?' backward-delete-char
+      bindkey -M viins '^H' backward-delete-char
+    '';
   };
 }
