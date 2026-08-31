b5c253ec61f60a44bf9280ce7cd2e2c05bd8a657
marvin <marvin@example.com>
2026-08-31 18:23:37 +0300

    set home-manager hyprland vrr=3

--- home-manager/configs/hyprland.lua
+++ home-manager/configs/hyprland.lua
@@ -217,7 +217,7 @@ hl.config({
     misc = {
         force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
         disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
-        vrr                     = 0,
+        vrr                     = 3,     -- 0 - off, 1 - on, 2 - fullscreen only, 3 - fullscreen with video or game content type
     },
 })
 
